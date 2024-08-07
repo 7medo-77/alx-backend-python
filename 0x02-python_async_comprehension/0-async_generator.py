e57fdcce62990903582f8a07957b9f0adbd120db
hmedo <hmedo@7medo>
2024-08-07 16:06:13 +0300

    0-async_generator.py_16-06

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Module containing generator yielding
+random number for 10 iterations
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Generator yielding random number for 10 iterations
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random()
