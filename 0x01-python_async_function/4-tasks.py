d8d8be34f5cbe3fb40787400fc7e023425f459fa
hmedo <hmedo@7medo>
2024-08-07 04:05:10 +0300

    __pycache___04-05

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module which includes a function that returns
+an array of sorted float numbers
+"""
+import asyncio
+import random
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Async function returning an
+    array of sorted float numbers
+    """
+    arrayFloat: List[float] = []
+
+    for _ in range(n):
+        arrayFloat.append(await task_wait_random(max_delay))
+
+    return sorted(arrayFloat)
